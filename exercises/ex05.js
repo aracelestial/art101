5235afbe658c06c9ac3eb03513d97d22083a4593
araceli <ara.rojasn@gmail.com>
2025-10-30 18:45:25 -0700

    explosion

--- exercises/ex05.js
+++ exercises/ex05.js
@@ -1,10 +1,22 @@
 let count = 0;
-let colors = ["orchid", "coral", "hot pink", "plum"]; 
+let colorIndex = 0;
+let mood;
+let colors = ["Orchid", "Coral", "HotPink", "Plum"];
 
-$("#needy-button").click(function() {
+$("#needy-button").click(function () {
+   if (count < 5) { mood = "fresh and happy"; }
+   else if ((count >= 5) && (count < 10)) { mood = "keep pushing"; }
+   else { mood = "so tired!"; }
 
-    $("#needy-button").html( "Clicks: " + count + " Color:" + colors[count] );
+   $("#needy-button").html("Clicks: " + count + " Color: " + colors[colorIndex] + " - " + mood);
+   $("body").css("background-color", colors[colorIndex]);
 
-    count = count +1;
+   $("body").append("<img width=50 src='images/orchidFlowers.png'>");
 
-    });
+
+   count = count + 1;
+   colorIndex = colorIndex + 1;
+
+   if (colorIndex == 4) { colorIndex = 0; } 
+
+});
