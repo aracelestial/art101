6919498f66435daf5d2be99a1dfa1e7e6a52877d
aracelestial <araceli.nulee@gmail.com>
2025-11-17 22:10:36 -0800

    Update lab08.js

--- exercises/lab08.js
+++ exercises/lab08.js
@@ -2,3 +2,8 @@ $("#creature").click(function() {
   $("#status").text("You clicked me! 🐾");
   $("#creature").css("background", "lavender");
 });
+
+$("#creature").hover(
+  function() { $("#status").text("You’re close... 👀"); },
+  function() { $("#status").text("You left me :("); }
+);
