c0ae9d42682f050ea1554b69d6ccb5c17878fe03
araceli <ara.rojasn@gmail.com>
2025-11-17 22:35:40 -0800

    update

--- exercises/lab08.js
+++ exercises/lab08.js
@@ -1,6 +1,6 @@
 $("#creature").click(function() {
   $("#status").text("You clicked me! 🐾");
-  $("#creature").css("background", "lavender");
+  $("#creature").css("background", "blue");
 });
 
 $("#creature").hover(function() {
